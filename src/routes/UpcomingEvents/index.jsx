06b1df19bd08f02dba48cc5eb6767fcf2c7b55f2
Youting <youting123456@gmail.com>
2022-07-29 12:57:45 +1200

    Created a form in upcoming events page to make events

--- src/routes/UpcomingEvents/index.jsx
+++ src/routes/UpcomingEvents/index.jsx
@@ -1,12 +1,14 @@
 import Heading from "./Heading";
 import EventBoxLeft from "./EventBoxLeft";
 import EventBoxRight from "./EventBoxRight";
+import EventForm from "./EventForm";
 
 export default function UpcomingEvents() {
   return (
     <div>
       <Heading />
       <div className="flex flex-col gap-8 p-8 justify-center items-center">
+        <EventForm />
         <EventBoxLeft />
         <EventBoxRight />
         <EventBoxLeft />
